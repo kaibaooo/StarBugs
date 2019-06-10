e882204d2e7c52bebea151e7999567ea4eca981b
ifTNT <happyjohn369@gmail.com>
2019-06-10 09:21:48 +0800

    Separate RecvBuffer from ClientHandler

--- src/com/github/nukcsie110/starbugs/server/ClientHandler.java
+++ src/com/github/nukcsie110/starbugs/server/ClientHandler.java
@@ -2,30 +2,30 @@ package com.github.nukcsie110.starbugs.server;
 
 import com.github.nukcsie110.starbugs.packet.Handler;
 import com.github.nukcsie110.starbugs.server.ServerUser;
+import com.github.nukcsie110.starbugs.server.RecvBuffer;
 import java.nio.channels.*;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class ClientHandler implements Handler {  
     private final static int BUF_SIZE=1024;
-    private ByteBuffer recvBuf;
-    private int readCnt;
-    private int exceptedPacketLength;
-    private byte pkID;
-    private int headOfPacket;
+    private RecvBuffer recvBuf;
+    //private int readCnt;
+    //private int exceptedPacketLength;
+    //private int headOfPacket;
     //private ByteBuffer packetBuf;
     private ByteBuffer writeBuf;
     private ServerUser player;  
       
     public ClientHandler(ServerUser _player) {  
     //public ClientHandler() {  
-        this.recvBuf = ByteBuffer.allocate(BUF_SIZE);
+        this.recvBuf = new RecvBuffer(BUF_SIZE);
         this.writeBuf = ByteBuffer.allocate(BUF_SIZE);
         //this.player = _player;
-        this.readCnt = 0;
-        this.exceptedPacketLength = -1;
+        //this.readCnt = 0;
+        //this.exceptedPacketLength = -1;
         //this.recvBuf.mark();
-        this.headOfPacket = 0;
+        //this.headOfPacket = 0;
     }  
       
     public void execute(Selector selector, SelectionKey key) {  
@@ -43,55 +43,14 @@ public class ClientHandler implements Handler {
       
     private void readPacket(Selector selector, SelectionKey key) throws IOException {  
         SocketChannel client = (SocketChannel) key.channel();  
-        int n = client.read(recvBuf);  
-        if(n==-1){ //Reach End-Of-Stream
-            key.interestOps(0);
+        if(!recvBuf.read(client)){
+            log("Close connection from: "+client.getRemoteAddress());
+            client.close();
+            client.keyFor(selector).cancel();
         }
-        //log("Recived: "+n+" bytes");
-        //printByteBuffer(recvBuf);
-        readCnt += n;
-        while(readCnt >= 5){
-            if(exceptedPacketLength==-1){
-                int oldPos = recvBuf.position();
-                recvBuf.position(headOfPacket); //Restore starting point of this packet
-                pkID = recvBuf.get();
-                exceptedPacketLength = recvBuf.getInt()+5;
-                recvBuf.position(oldPos); //Restore position
-                //log("Packet ID: " + pkID);
-                //log("Excepted packet length: " + exceptedPacketLength);
-                //log("Current recived length: " + readCnt);
-            }else{
-                //log("Excepted packet length: " + exceptedPacketLength);
-                //log("Current recived length: " + readCnt);
-                if(readCnt>=exceptedPacketLength){
-                    //Get whole packet
-                    recvBuf.position(headOfPacket); //Restore starting point of this packet
-                    //Read only expected length
-                    byte[] packet = new byte[exceptedPacketLength];
-                    log("Packet size: "+ exceptedPacketLength);
-                    recvBuf.get(packet);
-                    
-                    packetHandle(packet);
-                    
-                    headOfPacket = recvBuf.position(); //Mark the starting point of next packet
-                    recvBuf.limit(recvBuf.capacity()); //Restore the limit
-                    readCnt -= exceptedPacketLength;
-                    exceptedPacketLength = -1;
-
-                    //if no data in buffer
-                    if(readCnt == 0){
-                        recvBuf.clear();
-                        headOfPacket = recvBuf.position(); //Mark the starting point of next packet
-                    }
-                }else{
-                    break;
-                }
-            }
+        while(recvBuf.hasPacket()){
+            packetHandle(recvBuf.getPacket());
         }
-        /*if (n > 0) {  
-            buffer.flip();  
-            key.interestOps(SelectionKey.OP_WRITE);     // switch to OP_WRITE  
-        }*/ 
     }
 
     private void packetHandle(byte[] packet){
